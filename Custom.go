67eca18c47c1c8ed94640c0e1007cdd094fd0bdb
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-06-29 15:47:42 +1000

    add redirect response

--- Custom.go
+++ Custom.go
@@ -1,6 +1,10 @@
 package response
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
 
 //Custom return custom code and message
 func Custom(code int, message string) (events.APIGatewayProxyResponse, error) {
@@ -9,6 +13,23 @@ func Custom(code int, message string) (events.APIGatewayProxyResponse, error) {
 		IsBase64Encoded: false,
 		Headers: map[string]string{
 			"Content-Type":                "text/plain",
+			"Access-Control-Allow-Origin": "*",
+		},
+		StatusCode: code,
+	}, nil
+}
+
+//CustomJSON return custom code and message
+func CustomJSON(code int, responseObject interface{}) (events.APIGatewayProxyResponse, error) {
+	resByte, err := json.Marshal(responseObject)
+	if err != nil {
+		return ServerError(err)
+	}
+	return events.APIGatewayProxyResponse{
+		Body:            string(resByte[:]),
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
 			"Access-Control-Allow-Origin": "*",
 		},
 		StatusCode: code,
---
+++ Redirect.go
@@ -0,0 +1,29 @@
+package response
+
+import "github.com/aws/aws-lambda-go/events"
+
+//Redirect return 300 code
+func Redirect() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:            "Redirect",
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type":                "text/plain",
+			"Access-Control-Allow-Origin": "*",
+		},
+		StatusCode: 300,
+	}, nil
+}
+
+//MovedPermanently return 301 code
+func MovedPermanently() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:            "Moved Permanently",
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type":                "text/plain",
+			"Access-Control-Allow-Origin": "*",
+		},
+		StatusCode: 301,
+	}, nil
+}
