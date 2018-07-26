d53e9155af7b96444f5c315376abd99fafeb4dc4
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-26 16:25:58 +1000

    log error

--- ServerError.go
+++ ServerError.go
@@ -2,12 +2,13 @@ package response
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
 //ServerError return 500 code
 func ServerError(err error) (events.APIGatewayProxyResponse, error) {
-	_ = fmt.Errorf("ERROR: %v", err)
+	log.Print(fmt.Errorf("ERROR: %v", err))
 	return Custom(500, "Internal Server Error", nil)
 }
